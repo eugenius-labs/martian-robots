985180766b8849048105fa02410cc5ca37be9bf3
Danny Lee <dannyl33@gmail.com>
2019-10-02 12:44:14 +0100

    Added Semantic UI CSS and main app CSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import './App.css';
 class App extends React.Component {
 
     
 
     render(){
         return(
-            <div>
+            <div className="ui container">
                 <h1>Martian Robots</h1>
             </div>
         );
