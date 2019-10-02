6fa07ca4f1fc1ed0b0f6111f92169b23c6bb6fad
Danny Lee <dannyl33@gmail.com>
2019-10-02 12:54:21 +0100

    Added component for entering planet dimensions and command

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import CommandForm from './CommandForm/CommandForm';
 class App extends React.Component {
 
-    
+    state = { command: '', planetDimensions: '' };
+
+    onCommanderSubmit = (command,planetDimensions) => {
+
+        this.setState({ command: command, planetDimensions: planetDimensions});
+    }
 
     render(){
         return(
             <div className="ui container">
                 <h1>Martian Robots</h1>
+                <CommandForm onSubmit={this.onCommanderSubmit}/>
+                <div>{this.state.planetDimensions}</div>
+                <div>{this.state.command}</div>
             </div>
         );
     };
---
+++ src/components/CommandForm/CommandForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class CommandInput extends React.Component {
+
+    state = { command: '', planetDimensions: '' };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.command,this.state.planetDimensions);
+    }
+
+    render(){
+        return(
+            <div className="ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="ui field">
+                        <label>Planet Dimensions
+                        </label>
+                        <input type="text" value={this.state.planetDimensions} onChange={(e) => this.setState({ planetDimensions: e.target.value})}/>
+                    </div>
+                    <div className="ui field">
+                        <label>Command
+                        </label>
+                        <textarea type="text" value={this.state.command} onChange={(e) => this.setState({ command: e.target.value})}/>
+                    </div>
+                    <div className="ui field">
+                        <input type="submit" value="Submit"/>
+                    </div>
+                </form>
+            </div>
+        );
+    };
+}
+
+export default CommandInput;
