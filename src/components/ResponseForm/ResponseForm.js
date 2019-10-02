8643bd7b2727fab317276e15a6a621dad9a3c412
Danny Lee <dannyl33@gmail.com>
2019-10-02 13:16:42 +0100

    Added sample data text file and some basic error checking in component did update on ResponseForm

--- src/components/ResponseForm/ResponseForm.js
+++ src/components/ResponseForm/ResponseForm.js
@@ -9,6 +9,29 @@ class ResponseForm extends React.Component {
 
     }
 
+    componentDidUpdate(){
+        console.log("updating");
+        const dims = this.props.planetDimensions.split(' ');
+        const planetWidth = parseInt(dims[0]);
+        const planetHeight = parseInt(dims[1]);
+        if (planetWidth > 0 && planetHeight > 0){
+            const positions = "VALID INPUT";
+            if (this.props.command.length > 0 && this.state.positions !== positions){
+                this.setState({positions: positions});
+            }
+        }
+        else{
+            const error = "INVALID PLANET DIMENSIONS";
+            if (this.state.positions !== error){
+                this.setState({positions: error});
+            }
+        }
+            
+            
+        
+        //console.log(this.props.command);
+    }
+
     render(){
         return(
             <div className="ui segment">
